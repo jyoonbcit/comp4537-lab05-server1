6e2257584fe1f8325f7fb4a7a6923357319bd864
Jihoon Yoon <jihoonyoon0@gmail.com>
2024-03-19 11:08:47 -0700

    Fix get and post

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import patients from '../en/lang/messages/patient.js';
 import message from '../en/lang/messages/user.js';
 
-const endPointRoot = "https://comp4537-lab05-server2.vercel.app/patient"; // replace with server's endpoint
+const endPointRoot = "https://comp4537-lab05-server2.vercel.app";
 const xhr = new XMLHttpRequest();
 
 class Query {
     executeQuery() {
         const query = document.getElementById('queryInput').value.trim();
         const method = this.determineMethod(query);
-        xhr.open(method, endPointRoot, true);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4 && xhr.status === 200) {
@@ -23,6 +22,7 @@ class Query {
         if (method === 'POST') {
             xhr.send(JSON.stringify({ query: query }));
         } else if (method === 'GET') {
+            xhr.open(method, encodeURIComponent(query));
             console.log("Sending GET request");
             xhr.send();
         } else {
@@ -41,11 +41,9 @@ class Query {
         }
     }
 
-
-
     postQuery() {
         const insertQuery = `INSERT INTO patient (name, dateOfBirth) VALUES ${patients.map(patient => `('${patient.name}', '${patient.dateOfBirth}')`).join(', ')}`;
-        xhr.open("POST", endPointRoot, true);
+        xhr.open("POST", endPointRoot);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4 && xhr.status === 200) {
@@ -57,11 +55,8 @@ class Query {
                 document.getElementById("response").innerHTML = message.insertFail;
             }
         }
-
         xhr.send(JSON.stringify({ query: insertQuery }));
-
     }
-
 }
 
 const query = new Query();
